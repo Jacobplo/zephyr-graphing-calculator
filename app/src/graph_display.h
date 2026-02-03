a6be2a349bd701bdba7edb4953c3b984bea357e5
Jacob Plourde <jacob_plourde@hotmail.com>
2026-02-03 07:55:58 -0700

    Definitions for graph parameters

--- app/src/graph_display.h
+++ app/src/graph_display.h
@@ -8,8 +8,19 @@
 #include "misc/lv_timer.h"
 
 
-#define SCREEN_WIDTH    320
-#define SCREEN_HEIGHT   240
+#define SCREEN_WIDTH        320
+#define SCREEN_HEIGHT       240
+#define SCREEN_X_MID        (SCREEN_WIDTH / 2)
+#define SCREEN_Y_MID        (SCREEN_HEIGHT / 2)
+#define SCREEN_TICK_WIDTH_X (SCREEN_WIDTH / 20)
+#define SCREEN_TICK_WIDTH_Y (SCREEN_HEIGHT / 20)
+
+#define X_MAX           10
+#define X_MIN           -10
+#define Y_MAX           10
+#define Y_MIN           -10
+#define X_MID           ((X_MAX + X_MIN) / 2)
+#define Y_MID           ((Y_MAX + Y_MIN) / 2)
 
 
 int8_t display_init(void);
